ccbeb847e11e914b740215f455edd3533d230099
Sebastiano_Turco <55883823+sebone2201@users.noreply.github.com>
2025-04-18 18:18:16 +0200

    Update smokey_database_version.py

--- smokey_database_version.py
+++ smokey_database_version.py
@@ -364,8 +364,9 @@ def main():
     
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
+    chrome_options.binary_location = "/usr/bin/chromium"  # molto importante
     chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--window-size=1920,1080")
+    #chrome_options.add_argument("--window-size=1920,1080")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.binary_location = "/usr/bin/chromium"
     chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36")

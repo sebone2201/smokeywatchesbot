82ab49c1692bfe988dbc7c8d2a237c29d6fceb32
Sebastiano_Turco <55883823+sebone2201@users.noreply.github.com>
2025-04-18 11:29:22 +0200

    Update smokey_database_version.py

--- smokey_database_version.py
+++ smokey_database_version.py
@@ -12,7 +12,7 @@ from selenium.webdriver.chrome.service import Service
 
 #!/usr/bin/env python3.7
 
-import argparse
+
 #import requests
 #from bs4 import BeautifulSoup, Tag
 import json
@@ -23,34 +23,6 @@ import re
  
 from datetime import datetime, time
 import time as t
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--add", dest='name', help="name of new tracking to be added")
-parser.add_argument("--url", help="url for your new tracking's search query")
-parser.add_argument("--minPrice", help="minimum price for the query")
-parser.add_argument("--maxPrice", help="maximum price for the query")
-parser.add_argument("--delete", help="name of the search you want to delete")
-parser.add_argument('--refresh', '-r', dest='refresh', action='store_true', help="refresh search results once")
-parser.set_defaults(refresh=False)
-parser.add_argument('--daemon', '-d', dest='daemon', action='store_true', help="keep refreshing search results forever (default delay 120 seconds)")
-parser.set_defaults(daemon=False)
-parser.add_argument('--activeHour', '-ah', dest='activeHour', help="Time slot. Hour when to be active in 24h notation")
-parser.add_argument('--pauseHour', '-ph', dest='pauseHour', help="Time slot. Hour when to pause in 24h notation")
-parser.add_argument('--delay', dest='delay', help="delay for the daemon option (in seconds)")
-parser.set_defaults(delay=120)
-parser.add_argument('--list', dest='list', action='store_true', help="print a list of current trackings")
-parser.set_defaults(list=False)
-parser.add_argument('--short_list', dest='short_list', action='store_true', help="print a more compact list")
-parser.set_defaults(short_list=False)
-parser.add_argument('--tgoff', dest='tgoff', action='store_true', help="turn off telegram messages")
-parser.set_defaults(tgoff=False)
-parser.add_argument('--notifyoff', dest='win_notifyoff', action='store_true', help="turn off windows notifications")
-parser.set_defaults(win_notifyoff=False)
-#parser.add_argument('--addtoken', dest='token', help="telegram setup: add bot API token")
-parser.add_argument('--addchatid', dest='chatid', help="telegram setup: add bot chat id")
-
-args = parser.parse_args()
-
 #chat_id_list = []
 querie_list = []
 

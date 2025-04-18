27544420faf442492da309f3462bd267bbee72f2
Sebastiano_Turco <55883823+sebone2201@users.noreply.github.com>
2025-04-18 13:28:46 +0200

    Update smokey_database_version.py

--- smokey_database_version.py
+++ smokey_database_version.py
@@ -353,14 +353,16 @@ def main():
     ### Setup commands ###
     global win_notifyoff
     global driver
-    win_notifyoff = False
+    win_notifyoff = True
 
 
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--window-size=1920,1080")
+    #chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.binary_location = "/usr/bin/chromium"
     chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36")
 
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)

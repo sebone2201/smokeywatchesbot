40f3d46c34e80767064cb74f8cf3e6b8d53d9c03
Sebastiano_Turco <55883823+sebone2201@users.noreply.github.com>
2025-04-20 10:30:27 +0200

    Update smokey_database_version.py

--- smokey_database_version.py
+++ smokey_database_version.py
@@ -368,7 +368,7 @@ def main():
     chrome_options.add_argument("--no-sandbox")
     #chrome_options.add_argument("--window-size=1920,1080")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.binary_location = "/usr/bin/chromium"
+    #chrome_options.binary_location = "/usr/bin/google-chrome"
     chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36")
 
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
